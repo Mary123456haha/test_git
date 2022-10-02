b8f42084dda936127c38353d206ade2907ef8180
Mary123456haha <2606923366@qq.com>
2022-10-03 00:28:28 +0800

    v2.0

--- src/com/neuedu/domain/Student.java
+++ src/com/neuedu/domain/Student.java
@@ -3,5 +3,8 @@ package com.neuedu.domain;
 public class Student {
     public static void main(String[] args) {
         System.out.println(111);
+System.out.println(222);
+System.out.println(333);
+System.out.println(444);
     }
 }
