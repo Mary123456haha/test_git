043299b0d6601a2899b74f90a61527be6b298c44
Mary123456haha <2606923366@qq.com>
2022-10-03 00:45:40 +0800

    加多三行

--- src/com/neuedu/domain/Student.java
+++ src/com/neuedu/domain/Student.java
@@ -3,5 +3,9 @@ package com.neuedu.domain;
 public class Student {
     public static void main(String[] args) {
         System.out.println(111);
+        System.out.println(555);
+        System.out.println(666);
+        System.out.println(777);
+
     }
 }
